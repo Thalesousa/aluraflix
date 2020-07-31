1405de79080a7cbd19b15c161294ca5ffb386d4d
thalesousa <thalestjsb@gmail.com>
2020-07-31 19:07:11 -0300

    add url para localhost e remoto

--- src/pages/cadastro/Categoria/index.js
+++ src/pages/cadastro/Categoria/index.js
@@ -29,7 +29,9 @@ function CadastroCategoria() {
 
   useEffect(() => {
     setTimeout(() => {
-      const URL = 'http://localhost:8080/categorias';
+      const URL = window.location.hostname.includes('localhost')
+        ? 'http://localhost:8080/categorias'
+        : 'https://gameflix-thalesousa.herokuapp.com/categorias';
       fetch(URL)
         .then(async (respostaDoServidor) => {
           const resposta = await respostaDoServidor.json();
